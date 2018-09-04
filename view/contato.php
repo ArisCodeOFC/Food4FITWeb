8b6abbcb6a25db3c3cf62b58d2f0a68047cc7a71
WESLEY ALVES DA SILVA <17170129@CFP127ALUNO.LOCAL>
2018-09-04 16:30:45 -0300

    Página Fale Conosco

---
+++ view/contato.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Food 4fit</title>
+	<link rel="icon" type="image/png" href="assets/images/icons/favicon.png"/>
+	<link rel="stylesheet" href="assets/css/stylesheet.css">
+	<link rel="stylesheet" href="assets/css/mobile-stylesheet.css">
+	<link rel="stylesheet" href="assets/css/font-style.css">
+	<link rel="stylesheet" href="assets/css/contato.css">
+	<script src="assets/js/jquery-3.3.1.min.js"></script>
+	<script src="assets/js/scripts.js"></script>
+</head>
+<body>
+	<?php require_once("components/navbar.html"); ?><!-- BARRA DE NAVEGAÇÃO VIA PHP -->
+	<section class="main"><!-- CONTAINER-MÃE DO SITE -->
+        <h2 id='page-title' class="margin-left-auto margin-right-auto">ENTRE EM CONTATO</h2>
+        <div class="form-generic width-small margin-left-auto margin-right-auto margin-top-30px" >
+            <form class="form-generic-content">
+                <label for="txtnome" class="label-generic">Nome:</label>
+                <input id='txtnome' name="txtnome" class="input-generic">
+                <label for="txsobrenome" class="label-generic">Sobrenome:</label>
+                <input id='txsobrenome' name="txsobrenome" class="input-generic">
+                <label for="txtemail" class="label-generic">E-mail:</label>
+                <input id='txtemail' name="txtemail" class="input-generic">
+                <label for="txttelefone" class="label-generic">Telefone:</label>
+                <input id='txttelefone' name="txttelefone" class="input-generic">
+                <label for="txtcelular" class="label-generic">Celular:</label>
+                <input id='txtcelular' name="txtcelular" class="input-generic">
+                <label for="txtcelular" class="label-generic">O que deseja nos dizer:</label>
+                <textarea id='txtcomentario' name='txtcomentario' class="textarea-generic"></textarea>
+
+            </form>
+            <div class='btn-generic'>
+                <span>Enviar</span>
+            </div>
+        </div>
+	</section>
+	<?php require_once("components/footer.html"); ?><!-- RODAPÉ VIA PHP -->
+</body>
+</html>
