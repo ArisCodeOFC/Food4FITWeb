911aadf4d4edce9ec8f9e171e1b095b2dc88c54b
henryyzn <mrbubblerigod@gmail.com>
2018-09-05 11:16:41 -0300

    Página de login

---
+++ view/login.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Logue-se - Food 4fit</title>
+	<link rel="icon" type="image/png" href="assets/images/icons/favicon.png"/>
+	<link rel="stylesheet" href="assets/css/stylesheet.css">
+	<link rel="stylesheet" href="assets/css/mobile-stylesheet.css">
+	<link rel="stylesheet" href="assets/css/font-style.css">
+    <link rel="stylesheet" href="assets/css/keyframes.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+	<script src="assets/js/jquery-3.3.1.min.js"></script>
+	<script src="assets/js/scripts.js"></script>
+</head>
+<body>
+    <section class="main-login">
+        <div class="form-generic width-small margin-left-auto margin-right-auto">
+            <form class="form-generic-content">
+                <label for="txtlogin" class="label-generic margin-top-30px" style="color: #fff;">Login:</label>
+                <input type="text" name="txtlogin" id="txtlogin" class="input-generic">
+                <label for="txtsenha" class="label-generic" style="color: #fff;">Senha:</label>
+                <input type="password" name="txtsenha" id="txtsenha" class="input-generic">
+                <div class="btn-generic margin-left-auto">
+                    <span>Entrar</span>
+                </div>
+
+                <div class="login-form-separator"></div>
+
+                <div class="social-login-button facebook margin-bottom-15px">
+                    <span class="margin-left-30px margin-top-15px margin-bottom-15px">Login com Facebook</span>
+                </div>
+                <div class="social-login-button google">
+                    <span class="margin-left-30px margin-top-15px margin-bottom-15px">Login com Google</span>
+                </div>
+
+                <div class="login-form-separator"></div>
+
+                <a href="" class="login-link margin-bottom-30px">Cadastrar-me</a>
+            </form>
+        </div>
+
+        <span class="title-login">FOOD<br>4FIT</span>
+        <span class="subtitle-login">COMIDA FITNESS</span>
+	</section>
+</body>
+</html>
