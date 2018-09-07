9fd421d1abf7a45c0ca70c2c5b6a084b6c98e6d1
henryyzn <mrbubblerigod@gmail.com>
2018-09-07 18:44:31 -0300

    Página "Minhas Dietas"

--- view/promocoes.php
+++ view/promocoes.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Todos os Pratos - Food 4fit</title>
+	<title>Promoções - Food 4fit</title>
 	<link rel="icon" type="image/png" href="assets/images/icons/favicon.png"/>
 	<link rel="stylesheet" href="assets/css/stylesheet.css">
 	<link rel="stylesheet" href="assets/css/mobile-stylesheet.css">
