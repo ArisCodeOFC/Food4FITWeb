6c54a97a2165b3e78b1037fb8e4d9ee3a991611a
henryyzn <mrbubblerigod@gmail.com>
2018-09-07 02:37:26 -0300

    Página Fit Session

--- view/fit-session.php
+++ view/fit-session.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,18 +7,64 @@
 	<title>Fit Session - Food 4fit</title>
 	<link rel="icon" type="image/png" href="assets/images/icons/favicon.png"/>
 	<link rel="stylesheet" href="assets/css/stylesheet.css">
-	<link rel="stylesheet" href="assets/css/mobile-stylesheet.css">
 	<link rel="stylesheet" href="assets/css/font-style.css">
     <link rel="stylesheet" href="assets/css/keyframes.css">
 	<link rel="stylesheet" href="assets/css/fitsession.css">
+	<link rel="stylesheet" href="assets/css/mobile-stylesheet.css">
 	<script src="assets/js/jquery-3.3.1.min.js"></script>
 	<script src="assets/js/scripts.js"></script>
 </head>
 <body>
 	<?php require_once("components/navbar.html"); ?><!-- BARRA DE NAVEGAÇÃO VIA PHP -->
 	<section class="main"><!-- CONTAINER-MÃE DO SITE -->
-        <h2 id='page-title' class="margin-left-auto margin-right-auto">ENTRE EM CONTATO</h2>
-
+        <h2 id="page-title" class="margin-left-auto margin-right-auto">FIT SESSION</h2>
+        <p id="page-subtitle" class="margin-left-auto margin-right-auto">Agrupamos os melhores assuntos sobre o<br>tema fitness em um lugar só, dê uma olhada!</p>
+        <div class="fit-session-block">
+            <section class="fit-session-content-row padding-bottom-60px">
+                <div class="fit-session-content-column-text animate fadeInLeft fast">
+                    <h3>Personal Fitness</h3>
+                    <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
+                    <div class="btn-generic margin-top-30px">
+                        <span>Ver Mais</span>
+                    </div>
+                </div>
+                <div class="fit-session-content-column-image animate fadeInRight fast">
+                    <figure class="image">
+                        <img src="assets/images/fit/img2.png" alt="Personal Fitness">
+                    </figure>
+                </div>
+            </section>
+            <!-- ========================================================================== -->
+            <section class="fit-session-content-row padding-bottom-60px">
+                <div class="fit-session-content-column-image animate fadeInLeft fast">
+                    <figure class="image">
+                        <img src="assets/images/fit/img3.png" alt="Personal Fitness">
+                    </figure>
+                </div>
+                <div class="fit-session-content-column-text animate fadeInRight fast">
+                    <h3>Dicas de Saúde</h3>
+                    <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
+                    <div class="btn-generic margin-top-30px">
+                        <span>Ver Mais</span>
+                    </div>
+                </div>
+            </section>
+            <!-- ========================================================================== -->
+            <section class="fit-session-content-row">
+                <div class="fit-session-content-column-text animate fadeInLeft fast">
+                    <h3>Por que o Fitness?</h3>
+                    <p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.</p>
+                    <div class="btn-generic margin-top-30px">
+                        <span>Ver Mais</span>
+                    </div>
+                </div>
+                <div class="fit-session-content-column-image animate fadeInRight fast">
+                    <figure class="image">
+                        <img src="assets/images/fit/img1.png" alt="Personal Fitness">
+                    </figure>
+                </div>
+            </section>
+        </div>
 	</section>
 	<?php require_once("components/footer.html"); ?><!-- RODAPÉ VIA PHP -->
 </body>
