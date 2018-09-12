2559a1552a7293ea376309f9ba1a87c2de007a70
WESLEY ALVES DA SILVA <17170129@CFP127ALUNO.LOCAL>
2018-09-12 16:05:30 -0300

    Código JavaScript do CRUD no CMS reflatorado para uma melhor leitura

--- assets/js/cms/home.js
+++ assets/js/cms/home.js
@@ -25,149 +25,19 @@ $(document).ready(function() {
 
     $(document).on("mouseenter", "#sidebar.collapse nav a", function() {
         var offset = $(this).offset();
-        var texto = $(this).find(".label").contents().get(0).nodeValue;
-        $("#tooltip").text(texto).css({top: offset.top + 5, left: offset.left + 50}).show();
+        var texto = $(this)
+            .find(".label")
+            .contents()
+            .get(0).nodeValue;
+
+        $("#tooltip")
+            .text(texto)
+            .css({top: offset.top + 5, left: offset.left + 50})
+            .show();
     });
 
     $(document).on("mouseleave", "#sidebar.collapse nav a", function() {
         $("#tooltip").hide();
-    });
-
-    $(document).on("click", "#tabs [data-for]", function() {
-        openTab($(this).data("for"));
-    });
-
-    $(document).on("change", "[data-imagem-upload] input", function() {
-        if (this.files && this.files[0]) {
-            var reader = new FileReader();
-            var parent = $(this).parent();
-            reader.onload = function(event) {
-                var image = parent.find("img");
-                image.attr("src", event.target.result);
-            }
-
-            reader.readAsDataURL(this.files[0]);
-        }
-    });
-
-    $(document).on("click", "[data-form-submit]", function() {
-        $($(this).data("form-submit")).find("input[type='submit']").trigger("click");
-    });
-
-    $(document).on("submit", "[data-ajax-form]", function(event) {
-        event.preventDefault();
-        var form = $(this);
-        var formData = formToObject($(this).serializeArray());
-        form.find("[data-imagem-upload]").each(function() {
-            var input = $(this).find("input")[0];
-            if (input.files && input.files[0]) {
-                var file = input.files[0];
-                formData[$(input).attr("name")] = {
-                    fileName: file.name,
-                    fileSize: file.size,
-                    fileType: file.type,
-                    fileData: $(this).find("img").attr("src")
-                }
-            }
-        });
-
-        var url = $(this).data("ajax-new");
-        var isUpdate = false;
-        if (form.data("object")) {
-            formData = $.extend(form.data("object"), formData);
-            url = $(this).data("ajax-update").replace("{id}", formData.id);
-            isUpdate = true;
-        }
-
-        $.ajax({
-            type: isUpdate ? "PUT" : "POST",
-            url: url,
-            data: JSON.stringify(formData),
-            success: function(item) {
-                resetForm(form);
-                var template = buildTemplate(item || formData);
-                if (isUpdate) {
-                    $("#tabela-items .linha[data-id='" + formData.id + "']").replaceWith(template);
-                    abrirToast("Item atualizado com sucesso.");
-                } else {
-                    $("#tabela-items").append(template);
-                    abrirToast("Item inserido com sucesso.");
-                }
-            },
-
-            error: function(erro) {
-                abrirToast(erro.responseJSON.result);
-            }
-        });
-    });
-
-    $(document).on("click", "#tabela-items .editar", function() {
-        var id = $(this).parent().parent().data("id");
-        var url = $("#tabela-items").data("ajax-edit").replace("{id}", id);
-        var form = $($("#tabela-items").data("form"));
-        $.ajax({
-            type: "GET",
-            url: url,
-            success: function(item) {
-                for (var key in item) {
-                    var element = form.find("[name='" + key + "'], [data-bind='" + key + "']");
-                    if (element.is("[data-sceditor]")) {
-                        element.data("sceditor-instance").setWysiwygEditorValue(item[key]);
-                    } else if (element.is("[data-imagem-upload]")) {
-                        element.find("img").attr("src", "../" + item[key]);
-                    } else {
-                        element.val(item[key]);
-                    }
-                }
-
-                form.data("object", item);
-                $("[data-for='#container-form']").text("Editar Bloco");
-                openTab("#container-form");
-            },
-
-            error: function(erro) {
-                abrirToast(erro.responseJSON.result);
-            }
-        });
-    });
-
-    $(document).on("click", "#tabela-items .toggle", function() {
-        var button = $(this);
-        var id = button.parent().parent().data("id");
-        var url = $("#tabela-items").data("ajax-toggle").replace("{id}", id);
-        $.ajax({
-            type: "PUT",
-            url: url,
-            success: function() {
-                button.toggleClass("ativar").toggleClass("desativar");
-                abrirToast("Item " + (button.hasClass("ativar") ? "desativado" : "ativado") + " com sucesso.");
-            },
-
-            error: function(erro) {
-                abrirToast(erro.responseJSON.result);
-            }
-        });
-    });
-
-    $(document).on("click", "#tabela-items .excluir", function() {
-        var id = $(this).parent().parent().data("id");
-        var url = $("#tabela-items").data("ajax-delete").replace("{id}", id);
-        $.ajax({
-            type: "DELETE",
-            url: url,
-            success: function() {
-                $("#tabela-items .linha[data-id='" + id + "']").remove();
-                abrirToast("Item excluído com sucesso.");
-            },
-
-            error: function(erro) {
-                abrirToast(erro.responseJSON.result);
-            }
-        });
-    });
-
-    $(document).on("click", "[data-form-cancel]", function() {
-        resetForm($($(this).data("form-cancel")));
     });
 
     abrirPagina(location.hash.replace(/[#\/]/g, "") || "dashboard");
@@ -178,50 +48,258 @@ function abrirPagina(pagina) {
     $.get(pagina + ".php", function(conteudo) {
         $("#page-content").html(conteudo);
         $("#sidebar nav a").removeClass("active");
-        var texto = $("#sidebar nav a[data-page-load='" + pagina + "']").addClass("active").find(".label").contents().get(0).nodeValue;;
+        var texto = $("#sidebar nav a[data-page-load='" + pagina + "']")
+            .addClass("active")
+            .find(".label")
+            .contents()
+            .get(0).nodeValue;
+
         window.location.hash = "#/" + pagina;
         $("#titulo-pagina").text(texto);
-        onViewLoad();
+        new CrudService();
     });
 }
 
-/* Evento para quando uma nova view for carregada */
-function onViewLoad() {
-    $("[data-sceditor]").each(function() {
-        if (!$(this).data("sceditor-instance")) {
-            var instance = $(this).sceditor({
-                format: "xhtml",
-                style: "../assets/css/sceditor.default.min.css",
-                emoticons: {},
-                toolbarExclude: "emoticon,youtube,print,maximize,source,table,quote,code",
-                dateFormat: "day/month/year",
-                resizeEnabled: false
-            }).sceditor("instance");
+function CrudService() {
+    var crudService = {
+        init: function() {
+            crudService.instance = $("[data-crud-instance]");
+            crudService.formInstance = crudService.instance.find("[data-ajax-form]");
+            crudService.tabelaInstance = crudService.instance.find("[data-tabela]");
+            crudService.setup();
+        },
 
-            $(this).data("sceditor-instance", instance);
+        setup: function() {
+            crudService.instance.find("[data-sceditor]").each(function() {
+                if (!$(this).data("sceditor-instance")) {
+                    var instance = $(this).sceditor({
+                        format: "xhtml",
+                        style: "../assets/css/sceditor.default.min.css",
+                        emoticons: {},
+                        toolbarExclude: "emoticon,youtube,print,maximize,source,table,quote,code",
+                        dateFormat: "day/month/year",
+                        resizeEnabled: false
+                    }).sceditor("instance");
+
+                    $(this).data("sceditor-instance", instance);
+                }
+            });
+
+            crudService.instance.on("click", "#tabs [data-for]", crudService.changeTab);
+            crudService.instance.on("change", "[data-imagem-upload] input", crudService.imageUpload);
+            crudService.instance.on("click", "[data-form-submit]", crudService.submitForm);
+            crudService.instance.on("click", "[data-form-cancel]", crudService.cancelarForm);
+            crudService.formInstance.on("submit", crudService.onFormSubmit);
+            crudService.tabelaInstance.on("click", ".editar", crudService.editar);
+            crudService.tabelaInstance.on("click", ".toggle", crudService.toggle);
+            crudService.tabelaInstance.on("click", ".excluir", crudService.excluir);
+        },
+
+        imageUpload: function() {
+            if (this.files && this.files[0]) {
+                var reader = new FileReader();
+                var parent = $(this).parent();
+                reader.onload = function(event) {
+                    parent
+                        .find("img")
+                        .attr("src", event.target.result);
+                }
+
+                reader.readAsDataURL(this.files[0]);
+            }
+        },
+
+        changeTab: function() {
+            crudService.openTab($(this).data("for"));
+        },
+
+        openTab: function(target) {
+            crudService.instance
+                .find("#tabs-content>*")
+                .removeClass("active")
+                .filter(target)
+                .addClass("active");
+
+            crudService.instance
+                .find("#tabs span[data-for='" + target + "']")
+                .addClass("active")
+                .siblings()
+                .removeClass("active");
+        },
+
+        submitForm: function() {
+            crudService.formInstance
+                .find("input[type='submit']")
+                .trigger("click");
+        },
+
+        cancelarForm: function() {
+            crudService.resetForm();
+        },
+
+        onFormSubmit: function(event) {
+            event.preventDefault();
+            var formData = formToObject(crudService.formInstance.serializeArray());
+
+            crudService.formInstance.find("[data-imagem-upload]").each(function() {
+                var input = $(this).find("input")[0];
+                if (input.files && input.files[0]) {
+                    var file = input.files[0];
+                    formData[$(input).attr("name")] = {
+                        fileName: file.name,
+                        fileSize: file.size,
+                        fileType: file.type,
+                        fileData: $(this).find("img").attr("src")
+                    }
+                }
+            });
+
+            var url = crudService.formInstance.data("ajax-new");
+            var isUpdate = false;
+            if (crudService.formInstance.data("object")) {
+                formData = $.extend(crudService.formInstance.data("object"), formData);
+                url = crudService.formInstance
+                    .data("ajax-update")
+                    .replace("{id}", formData.id);
+
+                isUpdate = true;
+            }
+
+            $.ajax({
+                type: isUpdate ? "PUT" : "POST",
+                url: url,
+                data: JSON.stringify(formData),
+                success: function(item) {
+                    crudService.resetForm();
+                    var template = crudService.buildTemplate(item || formData);
+                    if (isUpdate) {
+                        crudService.tabelaInstance
+                            .find(".linha[data-id='" + formData.id + "']")
+                            .replaceWith(template);
+                        abrirToast("Item atualizado com sucesso.");
+                    } else {
+                        crudService.tabelaInstance.append(template);
+                        abrirToast("Item inserido com sucesso.");
+                    }
+                },
+
+                error: function(erro) {
+                    abrirToast(erro.responseJSON.result);
+                }
+            });
+        },
+
+        editar: function() {
+            var id = $(this).parent().parent().data("id");
+            var url = crudService.tabelaInstance
+                .data("ajax-edit")
+                .replace("{id}", id);
+
+            $.ajax({
+                type: "GET",
+                url: url,
+                success: function(item) {
+                    for (var key in item) {
+                        var element = crudService.formInstance.find("[name='" + key + "'], [data-bind='" + key + "']");
+                        if (element.is("[data-sceditor]")) {
+                            element
+                                .data("sceditor-instance")
+                                .setWysiwygEditorValue(item[key]);
+                        } else if (element.is("[data-imagem-upload]")) {
+                            element
+                                .find("img")
+                                .attr("src", "../" + item[key]);
+                        } else {
+                            element.val(item[key]);
+                        }
+                    }
+
+                    crudService.formInstance.data("object", item);
+                    $("[data-for='#container-form']").text("Editar Bloco");
+                    crudService.openTab("#container-form");
+                },
+
+                error: function(erro) {
+                    abrirToast(erro.responseJSON.result);
+                }
+            });
+        },
+
+        toggle: function() {
+            var button = $(this);
+            var id = button.parent().parent().data("id");
+            var url = crudService.tabelaInstance.data("ajax-toggle").replace("{id}", id);
+            $.ajax({
+                type: "PUT",
+                url: url,
+                success: function() {
+                    button
+                        .toggleClass("ativar")
+                        .toggleClass("desativar");
+                    abrirToast("Item " + (button.hasClass("ativar") ? "desativado" : "ativado") + " com sucesso.");
+                },
+
+                error: function(erro) {
+                    abrirToast(erro.responseJSON.result);
+                }
+            });
+        },
+
+        excluir: function() {
+            var id = $(this).parent().parent().data("id");
+            var url = crudService.tabelaInstance.data("ajax-delete").replace("{id}", id);
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function() {
+                    crudService.tabelaInstance
+                        .find(".linha[data-id='" + id + "']")
+                        .remove();
+                    abrirToast("Item excluído com sucesso.");
+                },
+
+                error: function(erro) {
+                    abrirToast(erro.responseJSON.result);
+                }
+            });
+        },
+
+        buildTemplate: function(item) {
+            var linha = $("<div class='linha' data-id='" + item.id + "'>");
+            $("<div class='coluna'>")
+                .append($("<img>")
+                        .attr("src", "../" + item.imagem)
+                        .attr("alt", item.titulo))
+                .appendTo(linha);
+            $("<div class='coluna titulo'>")
+                .append($("<span>").text(item.titulo))
+                .appendTo(linha);
+            $("<div class='coluna descricao'>")
+                .append($("<span>").text($(item.texto).text()))
+                .appendTo(linha);
+            $("<div class='coluna'>")
+                .html("<span class='toggle " + (item.ativo ? "desativar" : "ativar") +  "'></span><hr><span class='editar'></span><hr><span class='excluir'></span>")
+                .appendTo(linha);
+            return linha;
+        },
+
+        resetForm: function() {
+            crudService.formInstance[0].reset();
+            crudService.formInstance
+                .find("[data-sceditor]")
+                .data("sceditor-instance")
+                .setWysiwygEditorValue("");
+            crudService.formInstance
+                .find("[data-imagem-upload] img")
+                .removeAttr("src");
+            crudService.formInstance.removeData("object");
+            crudService.instance
+                .find("[data-for='#container-form']")
+                .text("Adicionar Bloco");
+            crudService.openTab("#container-listagem");
         }
-    })
-}
+    };
 
-function openTab(target) {
-    $("#tabs-content>*").removeClass("active").filter(target).addClass("active");
-    $("#tabs span[data-for='" + target + "']").addClass("active").siblings().removeClass("active");
-}
-
-function buildTemplate(item) {
-    var linha = $("<div class='linha' data-id='" + item.id + "'>");
-    $("<div class='coluna'>").append($("<img>").attr("src", "../" + item.imagem).attr("alt", item.titulo)).appendTo(linha);
-    $("<div class='coluna titulo'>").append($("<span>").text(item.titulo)).appendTo(linha);
-    $("<div class='coluna descricao'>").append($("<span>").text($(item.texto).text())).appendTo(linha);
-    $("<div class='coluna'>").html("<span class='toggle " + (item.ativo ? "desativar" : "ativar") +  "'></span><hr><span class='editar'></span><hr><span class='excluir'></span>").appendTo(linha);
-    return linha;
-}
-
-function resetForm(form) {
-    form[0].reset();
-    form.find("[data-sceditor]").data("sceditor-instance").setWysiwygEditorValue("");
-    form.find("[data-imagem-upload] img").removeAttr("src");
-    form.removeData("object");
-    $("[data-for='#container-form']").text("Adicionar Bloco");
-    openTab("#container-listagem");
+    crudService.init();
+    return crudService;
 }
--- view/cms/sobre.php
+++ view/cms/sobre.php
@@ -3,69 +3,70 @@
     $itens = SobreNosController::listar();
 ?>
 
-<div id="tabs">
-    <span data-for="#container-form">Adicionar Bloco</span>
-    <span class="active" data-for="#container-listagem">Listar Blocos</span>
-</div>
-<div id="tabs-content">
-    <div id="container-form">
-        <div class="form-generic">
-            <form id="form-sobre-nos" class="form-generic-content"
-               data-ajax-form
-               data-ajax-new="../api/v1/sobre-nos"
-               data-ajax-update="../api/v1/sobre-nos/{id}">
-                <label for="titulo" class="label-generic">Título:</label>
-                <input id="titulo" name="titulo" class="input-generic" required maxlength="255">
-                <label for="texto" class="label-generic">Texto:</label>
-                <textarea id="texto" name="texto" class="textarea-generic" data-sceditor></textarea>
-                <span class="label-generic">Imagem:</span>
-                <div class="imagem-upload-wrapper" data-imagem-upload data-bind="imagem">
-                    <div>
-                        <img alt="">
+<div data-crud-instance>
+    <div id="tabs">
+        <span data-for="#container-form">Adicionar Bloco</span>
+        <span class="active" data-for="#container-listagem">Listar Blocos</span>
+    </div>
+    <div id="tabs-content">
+        <div id="container-form">
+            <div class="form-generic">
+                <form id="form-sobre-nos" class="form-generic-content"
+                   data-ajax-form
+                   data-ajax-new="../api/v1/sobre-nos"
+                   data-ajax-update="../api/v1/sobre-nos/{id}">
+                    <label for="titulo" class="label-generic">Título:</label>
+                    <input id="titulo" name="titulo" class="input-generic" required maxlength="255">
+                    <label for="texto" class="label-generic">Texto:</label>
+                    <textarea id="texto" name="texto" class="textarea-generic" data-sceditor></textarea>
+                    <span class="label-generic">Imagem:</span>
+                    <div class="imagem-upload-wrapper" data-imagem-upload data-bind="imagem">
+                        <div>
+                            <img alt="">
+                        </div>
+                        <label for="imagem" class="label-generic">Selecione um arquivo...</label>
+                        <input id="imagem" name="uploadData" type="file" accept="image/*">
                     </div>
-                    <label for="imagem" class="label-generic">Selecione um arquivo...</label>
-                    <input id="imagem" name="uploadData" type="file" accept="image/*">
-                </div>
-                <input type="submit" name="submit">
-            </form>
-            <div class="controls">
-                <span class="cancel" data-form-cancel="#form-sobre-nos">Cancelar</span>
-                <div class="btn-generic">
-                    <span data-form-submit="#form-sobre-nos">Enviar</span>
+                    <input type="submit" name="submit">
+                </form>
+                <div class="controls">
+                    <span class="cancel" data-form-cancel>Cancelar</span>
+                    <div class="btn-generic">
+                        <span data-form-submit>Enviar</span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="active" id="container-listagem">
-        <div id="tabela-items"
-           data-form="#form-sobre-nos"
-           data-ajax-edit="../api/v1/sobre-nos/{id}"
-           data-ajax-delete="../api/v1/sobre-nos/{id}"
-           data-ajax-toggle="../api/v1/sobre-nos/{id}/ativar">
-            <div class="linha">
-                <div class="coluna">Imagem</div>
-                <div class="coluna titulo">Título</div>
-                <div class="coluna descricao">Descrição</div>
-                <div class="coluna">Opções</div>
-            </div>
-            <?php foreach ($itens as $item) { ?>
-                <div class="linha" data-id="<?= $item->id ?>">
-                    <div class="coluna">
-                        <img src="../<?= $item->imagem ?>" alt="<?= $item->titulo ?>">
-                    </div>
-                    <div class="coluna titulo"><span><?= $item->titulo ?></span></div>
-                    <div class="coluna descricao">
-                        <span>
-                            <?= strip_tags($item->texto) ?>
-                        </span>
-                    </div>
-                    <div class="coluna">
-                        <span class="toggle <?= $item->ativo ? "desativar" : "ativar" ?>"></span><hr>
-                        <span class="editar"></span><hr>
-                        <span class="excluir"></span>
-                    </div>
+        <div class="active" id="container-listagem">
+            <div id="tabela-items" data-tabela
+               data-ajax-edit="../api/v1/sobre-nos/{id}"
+               data-ajax-delete="../api/v1/sobre-nos/{id}"
+               data-ajax-toggle="../api/v1/sobre-nos/{id}/ativar">
+                <div class="linha">
+                    <div class="coluna">Imagem</div>
+                    <div class="coluna titulo">Título</div>
+                    <div class="coluna descricao">Descrição</div>
+                    <div class="coluna">Opções</div>
                 </div>
-            <?php } ?>
+                <?php foreach ($itens as $item) { ?>
+                    <div class="linha" data-id="<?= $item->id ?>">
+                        <div class="coluna">
+                            <img src="../<?= $item->imagem ?>" alt="<?= $item->titulo ?>">
+                        </div>
+                        <div class="coluna titulo"><span><?= $item->titulo ?></span></div>
+                        <div class="coluna descricao">
+                            <span>
+                                <?= strip_tags($item->texto) ?>
+                            </span>
+                        </div>
+                        <div class="coluna">
+                            <span class="toggle <?= $item->ativo ? "desativar" : "ativar" ?>"></span><hr>
+                            <span class="editar"></span><hr>
+                            <span class="excluir"></span>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
         </div>
     </div>
 </div>
