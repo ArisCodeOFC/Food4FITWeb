e2133ce696827a47844c9c7ae740c105a6649c32
henryyzn <mrbubblerigod@gmail.com>
2018-09-24 01:58:31 -0300

    Otimizações e wireframes

--- view/homenagem-parceiros.php
+++ view/homenagem-parceiros.php
@@ -23,7 +23,27 @@
 	<?php require_once("components/navbar.html"); ?><!-- BARRA DE NAVEGAÇÃO VIA PHP -->
 	<section class="main"><!-- CONTAINER-MÃE DO SITE -->
         <h2 id="page-title">HOMENAGEM À PARCEIROS</h2>
-        <p id="page-subtitle">Conheça os nossos laços que foram e são <br> essenciais para que a Food 4Fit cresça <br> cada vez mais a cada dia!</p>
+        <p id="page-subtitle" class="padding-bottom-25px">Conheça os nossos laços que foram e são <br> essenciais para que a Food 4Fit cresça <br> cada vez mais a cada dia!</p>
+        <div class="contact-buddy-button width-500px margin-auto" id="abrir">
+            <span>ENTRE EM CONTATO</span>
+        </div>
+        <div class="form-generic width-500px hide margin-left-auto margin-right-auto margin-top-30px">
+            <form class="form-generic-content" id="form-contato">
+                <label for="titulo" class="label-generic">Título:</label>
+                <input type="text" id="titulo" name="titulo" class="input-generic" placeholder="Digite um título..." required>
+
+                <label for="descricao" class="label-generic">Descrição:</label>
+                <textarea id="descricao" name="descricao" class="textarea-generic" required></textarea>
+
+                <label for="link" class="label-generic">Link:</label>
+                <input type="text" id="link" name="link" class="input-generic" placeholder="Digite o link do seu site..." required>
+
+                <input type="submit" class="display-none">
+            </form>
+            <div class="btn-generic margin-bottom-30px" data-f4f-form-submit="#form-contato">
+                <span>Enviar</span>
+            </div>
+        </div>
         <div class="generic-grid animate fadeInUp">
             <?php
                 for($i = 1; $i < 10; $i++){
--- view/login.php
+++ view/login.php
@@ -19,32 +19,35 @@
 	<script src="assets/public/js/jquery-3.3.1.min.js"></script>
 	<script src="assets/js/scripts.js"></script>
 </head>
-<body class=" login-bg">
+<body class="login-bg">
     <section class="main-login">
         <h2 class="title-login">FOOD<br>4FIT</h2>
         <h3 class="subtitle-login">COMIDA FITNESS</h3>
         <div class="form-generic width-400px margin-left-auto margin-right-auto">
             <form class="form-generic-content">
                 <label for="login" class="label-generic margin-top-30px" style="color: #fff;">Login:</label>
-                <input type="text" name="login" id="login" class="input-generic">
+                <input type="text" name="login" id="login" class="input-generic" style="color: #fff;">
                 <label for="senha" class="label-generic" style="color: #fff;">Senha:</label>
-                <input type="password" name="senha" id="senha" class="input-generic">
-                <div class="btn-generic margin-left-auto">
-                    <span>Entrar</span>
+                <input type="password" name="senha" id="senha" class="input-generic" style="color: #fff;">
+                <div class="margin-top-30px margin-bottom-30px form-row">
+                    <span class="margin-right-15px">Esqueci minha senha</span>
+                    <div class="btn-generic">
+                        <span>Entrar</span>
+                    </div>
                 </div>
 
                 <div class="login-form-separator"></div>
 
                 <div class="social-login-button facebook margin-bottom-15px">
-                    <span class="margin-left-30px margin-top-15px margin-bottom-15px">Login com Facebook</span>
+                    <span class="margin-left-15px margin-top-15px margin-bottom-15px">Login com Facebook</span>
                 </div>
                 <div class="social-login-button google">
-                    <span class="margin-left-30px margin-top-15px margin-bottom-15px">Login com Google</span>
+                    <span class="margin-left-15px margin-top-15px margin-bottom-15px">Login com Google</span>
                 </div>
 
                 <div class="login-form-separator"></div>
 
-                <a href="" class="login-link margin-bottom-30px">Cadastrar-me</a>
+                <a href="cadastro-usuario.php" class="login-link margin-bottom-30px">Não tem uma conta? <span>Cadastre-se.</span></a>
             </form>
         </div>
 	</section>
