a9e5c3655f3e47e9f449ddce9d362cb006301768
mauricio <mauriciotakeshi@hotmail.com>
2018-09-27 11:35:10 -0300

    Dashboard transformado em SPA (CMS)

--- view/cms/dashboard.php
+++ view/cms/dashboard.php
@@ -1,40 +1,27 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-	    <link rel="stylesheet" href="../assets/css/font-style.css">
-        <link rel="stylesheet" href="../assets/css/bases.css">
-        <link rel="stylesheet" href="../assets/css/sizes.css">
-        <link rel="stylesheet" href="../assets/css/align.css">
-        <link rel="stylesheet" href="../assets/css/keyframes.css">
-        <link rel="stylesheet" href="../assets/css/cms/stylesheet-cms.css">
-    </head>
-    <body>
-        <div class="dash-wrapper">
-            <div class="one card-dash">
-                <h2 class="title padding-top-20px padding-bottom-5px padding-left-20px">Bem Vindo!</h2>
-                <span class="subtitle padding-left-20px">O serviço desktop pensado para facilitar a sua vida.</span>
-                <p class="padding-left-20px">
-                    Este software tem como objetivo controlar a área financeira e a administração geral da empresa Food 4Fit.
-                    <br>
-                    Caso ache necessário, veja o guia de ajuda de interface disponível logo abaixo para aprender sobre o software.
-                </p>
-                <div class="btn-generic margin-left-20px margin-bottom-20px">
-                    <span>Ler Mais</span>
-                </div>
-            </div>
-            <div class="two card-dash">
-                <h2 class="title padding-top-20px padding-bottom-5px padding-left-20px">Mais Comprado Este Mês</h2>
-                <span class="subtitle padding-left-20px">Produto de Nome X</span>
-            </div>
-            <div class="three card-dash">
-                <p>a</p>
-            </div>
-            <div class="four card-dash">
-                <p>a</p>
-            </div>
-            <div class="five card-dash">
-                <p>a</p>
-            </div>
+<div class="dash-wrapper">
+    <div class="one card-dash">
+        <h2 class="title padding-top-20px padding-bottom-5px padding-left-20px">Bem Vindo!</h2>
+        <span class="subtitle padding-left-20px">O serviço desktop pensado para facilitar a sua vida.</span>
+        <p class="padding-left-20px">
+            Este software tem como objetivo controlar a área financeira e a administração geral da empresa Food 4Fit.
+            <br>
+            Caso ache necessário, veja o guia de ajuda de interface disponível logo abaixo para aprender sobre o software.
+        </p>
+        <div class="btn-generic margin-left-20px margin-bottom-20px">
+            <span>Ler Mais</span>
         </div>
-    </body>
-</html>
+    </div>
+    <div class="two card-dash">
+        <h2 class="title padding-top-20px padding-bottom-5px padding-left-20px">Mais Comprado Este Mês</h2>
+        <span class="subtitle padding-left-20px">Produto de Nome X</span>
+    </div>
+    <div class="three card-dash">
+        <p>a</p>
+    </div>
+    <div class="four card-dash">
+        <p>a</p>
+    </div>
+    <div class="five card-dash">
+        <p>a</p>
+    </div>
+</div>
