72d38684bd1dc5ebecb6156320d15994d47e1136
Allan Alves <allanalves1904@gmail.com>
2018-10-03 00:36:10 -0300

    ignorem

--- view/cms/pratos.php
+++ view/cms/pratos.php
@@ -1,10 +1,3 @@
-<style>
-    .pratos-wrapper{
-        width: 100%;
-        height: auto;
-        background-color: aquamarine;
-    }
-</style>
 <div class="pratos-wrapper">
      <div id="page-actions">
         <a href="#" id="btn-adicionar-ingrediente">
@@ -16,5 +9,23 @@
             <span>Recarregar Listagem</span>
         </a>
     </div>
+    <div class="generic-grid animate fadeInUp">
+        <div class="generic-card">
+            <img src="../assets/images/dishs/img1.jpg" alt="Teste" class="generic-card-img">
+            <div class="generic-card-overlay">
+                <span class="card-dish-name margin-bottom-20px">Frango Com Batatas</span>
+                <div class="card-dish-separator margin-bottom-15px"></div>
+                <p class="card-dish-description margin-bottom-30px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni magnam saepe reiciendis.</p>
+                <div class="card-dish-price margin-bottom-30px"><!-- CONTAINER DO PREÇO DO PRATO NA INDEX -->
+                    <span class="padding-right-15px">R$ 9,90</span><!-- PREÇO -->
+                    <div><img src="assets/images/simbols/delivery-truck.svg" alt="Compra Rápida"></div><!-- COMPRA RAPIDA -->
+                </div>
 
+                <div class="edit-btns">
+                    <img src="assets/images/icons/edit.svg" alt="Editar Prato">
+                    <img src="assets/images/icons/delete-white.svg" alt="Excluir Prato">
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
