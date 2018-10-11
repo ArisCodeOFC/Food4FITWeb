430fecaecb2b7b2d921022a493b9997f88a47053
henryyzn <mrbubblerigod@gmail.com>
2018-10-11 07:09:21 -0300

    Login sem funcionar

--- view/cms/login.php
+++ view/cms/login.php
@@ -65,24 +65,22 @@
 	    <script src="../assets/js/cms/main.js"></script>
 	    <script src="../assets/js/cms/login.controller.js"></script>
     </head>
-    <body>
-        <div class="main-login">
-            <article class="image-bg-login">
-                <h2>FOOD<br>4FIT<span>COMIDA FITNESS</span></h2>
-            </article>
-            <div class="form-login-wrapper">
-                <figure>
-                    <img src="" alt="">
-                </figure>
-                <form id="form-login" class="width-550px margin-auto padding-top-60px" autocomplete="off">
-                    <input type="text" name="matricula" placeholder="Matrícula" class="margin-bottom-20px" autocomplete="off" required>
-                    <input type="password" name="senha" autocomplete="off" id="senha" placeholder="Senha" required>
-                    <div id="rodape">
-                        <a href="nao-tenho-conta.php">Não tenho uma conta</a>
-                        <input type="submit" value="Entrar">
-                    </div>
-                </form>
-            </div>
+    <body class="main-login" data-controller="LoginController">
+        <article class="image-bg-login">
+            <h2>FOOD<br>4FIT<span>COMIDA FITNESS</span></h2>
+        </article>
+        <div class="form-login-wrapper">
+            <figure>
+                <img src="" alt="">
+            </figure>
+            <form id="form-login" class="width-550px margin-auto padding-top-60px" autocomplete="off">
+                <input type="text" name="matricula" placeholder="Matrícula" class="margin-bottom-20px" autocomplete="off" required>
+                <input type="password" name="senha" autocomplete="off" id="senha" placeholder="Senha" required>
+                <div id="rodape">
+                    <a href="nao-tenho-conta.php">Não tenho uma conta</a>
+                    <input type="submit" value="Entrar">
+                </div>
+            </form>
         </div>
     </body>
 </html>
